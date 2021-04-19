1b9fff52c499bcfdff35f8a423b138ba0d384b2c
Team DT <79621444+projectDT@users.noreply.github.com>
2021-04-19 09:34:38 +0530

    Update 1 StringPractice.java

--- 1 StringPractice.java
+++ 1 StringPractice.java
@@ -4,7 +4,7 @@ public class StringPractice {
 
     public static void main(String[] args) {
         
-		/*int a=10;//a=-10
+	/*int a=10;//a=-10
         System.out.printf("%05d",a);*/
         
         /*float a=123.45f;//3.45f
